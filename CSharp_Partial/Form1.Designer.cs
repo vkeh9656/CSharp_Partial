487c08d6acb43b9b727ac71d9f608e846d90499d
vkeh9656 <37315637+vkeh9656@users.noreply.github.com>
2023-05-22 18:41:13 +0900

    UI 세팅

--- CSharp_Partial/Form1.Designer.cs
+++ CSharp_Partial/Form1.Designer.cs
@@ -28,20 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lboxItem = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboxItem = new System.Windows.Forms.ComboBox();
+            this.cboxRate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numCount = new System.Windows.Forms.NumericUpDown();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tboxResult = new System.Windows.Forms.TextBox();
+            this.tboxErrorMsg = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numCount)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lboxItem
+            // 
+            this.lboxItem.FormattingEnabled = true;
+            this.lboxItem.ItemHeight = 12;
+            this.lboxItem.Location = new System.Drawing.Point(12, 12);
+            this.lboxItem.Name = "lboxItem";
+            this.lboxItem.Size = new System.Drawing.Size(210, 196);
+            this.lboxItem.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "상품";
+            // 
+            // cboxItem
+            // 
+            this.cboxItem.FormattingEnabled = true;
+            this.cboxItem.Location = new System.Drawing.Point(230, 27);
+            this.cboxItem.Name = "cboxItem";
+            this.cboxItem.Size = new System.Drawing.Size(121, 20);
+            this.cboxItem.TabIndex = 2;
+            // 
+            // cboxRate
+            // 
+            this.cboxRate.FormattingEnabled = true;
+            this.cboxRate.Location = new System.Drawing.Point(230, 81);
+            this.cboxRate.Name = "cboxRate";
+            this.cboxRate.Size = new System.Drawing.Size(121, 20);
+            this.cboxRate.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "할인율(%)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "개수";
+            // 
+            // numCount
+            // 
+            this.numCount.Location = new System.Drawing.Point(231, 138);
+            this.numCount.Name = "numCount";
+            this.numCount.Size = new System.Drawing.Size(120, 21);
+            this.numCount.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(228, 185);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(123, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "담기";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // tboxResult
+            // 
+            this.tboxResult.Location = new System.Drawing.Point(12, 214);
+            this.tboxResult.Name = "tboxResult";
+            this.tboxResult.Size = new System.Drawing.Size(210, 21);
+            this.tboxResult.TabIndex = 8;
+            // 
+            // tboxErrorMsg
+            // 
+            this.tboxErrorMsg.ForeColor = System.Drawing.Color.Red;
+            this.tboxErrorMsg.Location = new System.Drawing.Point(12, 241);
+            this.tboxErrorMsg.Name = "tboxErrorMsg";
+            this.tboxErrorMsg.Size = new System.Drawing.Size(339, 21);
+            this.tboxErrorMsg.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 299);
+            this.ClientSize = new System.Drawing.Size(368, 281);
+            this.Controls.Add(this.tboxErrorMsg);
+            this.Controls.Add(this.tboxResult);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.numCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboxRate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboxItem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lboxItem);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numCount)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lboxItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboxItem;
+        private System.Windows.Forms.ComboBox cboxRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numCount;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox tboxResult;
+        private System.Windows.Forms.TextBox tboxErrorMsg;
     }
 }
 
